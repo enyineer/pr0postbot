7f957ccc2a38ba70c2f43b01e0621db14e793c02
Nico <nico.enking@gmail.com>
2022-04-16 02:27:44 +0200

    Print timestamp

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -8,11 +8,12 @@ export class Logger {
         if (Logger.instance === undefined) {
             Logger.instance = winston.createLogger({
                 level: 'info',
-                format: winston.format.json(),
+                format: winston.format.combine(
+                    winston.format.timestamp(),
+                    winston.format.json()
+                ),
                 transports: [
-                    new winston.transports.Console({
-                        format: winston.format.simple()
-                    })
+                    new winston.transports.Console()
                 ],
             });
         }
