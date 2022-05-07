e28eccf17b8a6b2ad2fdf9d0ffb62a6c08f60ade
Nico <nico.enking@gmail.com>
2022-05-07 16:52:56 +0200

    Filter out all messages that are older than last update

--- src/services/logic/chatService.ts
+++ src/services/logic/chatService.ts
@@ -100,8 +100,8 @@ export class ChatService {
                 .filterByBenis(chat.minBenis)
                 // Filter out all items that have already been shown
                 .removeIds(shownItemIds)
-                // Filter out all items that are older than the chat
-                .filterNewerThan(DateTime.fromJSDate(chat.createdAt))
+                // Filter out all items that are older than the last update
+                .filterNewerThan(DateTime.fromJSDate(chat.lastUpdate))
                 // Filter out all items that don't match the chats flags
                 .filterByFlags({
                     sfw: chat.sfw,
