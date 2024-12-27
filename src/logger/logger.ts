defacf0945072275877dcd4a4d5d22a5bfda1ea2
enyineer <nico.enking@gmail.com>
2024-12-27 17:05:52 +0100

    Write timestamp to console

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,34 +1,34 @@
-import winston from "winston"
+import winston from "winston";
 
 export class Logger {
+  private static instance: winston.Logger;
 
-    private static instance: winston.Logger;
-
-    static get i(): winston.Logger {
-        if (Logger.instance === undefined) {
-            Logger.instance = winston.createLogger({
-                level: 'info',
-                transports: [
-                    new winston.transports.Console({
-                        format: winston.format.simple(),
-                        level: 'debug'
-                    }),
-                    new winston.transports.File({
-                        filename: 'pr0postbot.log',
-                        format: winston.format.combine(
-                            winston.format.timestamp(),
-                            winston.format.json()
-                        ),
-                    })
-                ],
-            });
-        }
-        return Logger.instance;
+  static get i(): winston.Logger {
+    if (Logger.instance === undefined) {
+      Logger.instance = winston.createLogger({
+        level: "info",
+        transports: [
+          new winston.transports.Console({
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              winston.format.simple()
+            ),
+            level: "debug",
+          }),
+          new winston.transports.File({
+            filename: "pr0postbot.log",
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              winston.format.json()
+            ),
+          }),
+        ],
+      });
     }
+    return Logger.instance;
+  }
 
-    private constructor() {
-        // Do nothing
-    }
-
-
+  private constructor() {
+    // Do nothing
+  }
 }
